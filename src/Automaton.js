aef00629aa4d6d283e82c5e3f075f2db961e10dd
Ruben9922 <Ruben9922@users.noreply.github.com>
2020-11-29 22:28:42 +0000

    Create basic app allowing inputting an automaton

---
+++ src/Automaton.js
@@ -0,0 +1,9 @@
+class Automaton {
+    constructor() {
+        this.alphabet = [];
+        this.states = [];
+        this.transitionFunction = new Map();
+        this.initialState = null;
+        this.finalStates = [];
+    }
+}

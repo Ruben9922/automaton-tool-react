0c0ce611b1b5d98f93a74541701f3a95e9509b04
Ruben9922 <Ruben9922@users.noreply.github.com>
2021-01-02 23:00:34 +0000

    Rename variable

--- src/Input.js
+++ src/Input.js
@@ -306,14 +306,14 @@ export default function Input({addAutomaton, onSnackbarOpenChange}) {
     const allStepsCompleted = () => steps.every(step => step.get("completed") === true);
 
     const handleNext = () => {
-        const newActiveStep =
+        const updatedActiveStepIndex =
             activeStepIndex === steps.count() - 1 && !allStepsCompleted()
                 ? // It's the last step, but not all steps have been completed
                   // find the first step that has not been completed
                 steps.findIndex(step => step.get("completed") === false)
                 : activeStepIndex + 1;
 
-        setActiveStepIndex(newActiveStep);
+        setActiveStepIndex(updatedActiveStepIndex);
     };
 
     const handleBack = () => setActiveStepIndex(prevActiveStep => prevActiveStep - 1);

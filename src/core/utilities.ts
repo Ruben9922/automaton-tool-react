fe11a198f863921ce2d094512f356c12ce8eb5a9
Ruben9922 <Ruben9922@users.noreply.github.com>
2021-03-20 21:52:58 +0000

    Fix isSubset() function

--- src/core/utilities.ts
+++ src/core/utilities.ts
@@ -13,4 +13,4 @@ export function isUniqueList<T>(p: (s1: T, s2: T) => boolean, l: T[]) {
   return R.map((s1) => R.equals(1, R.length(R.filter(R.partial(p, [s1]), l))), l);
 }
 
-export const isSubset: <T>(l1: T[], l2: T[]) => boolean = R.uncurryN(2, R.pipe(R.without, R.empty));
+export const isSubset: <T>(l1: T[], l2: T[]) => boolean = R.flip(R.pipe(R.without, R.isEmpty));
